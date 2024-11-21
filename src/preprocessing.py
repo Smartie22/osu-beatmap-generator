c20d645f16b76e87699a0f6939e86542ee8ced07
Seb <sebastianpedrucochoi@hotmail.com>
2024-11-21 12:54:42 -0500

    implemented length, getitem, added comments and todos for preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,6 +3,7 @@ This module will contain the preprocessers that will preprocess all over the dat
 '''
 import os
 from torch.utils.data import Dataset
+from torch import tensor
 import librosa
 import numpy as np
 
@@ -10,30 +11,74 @@ class BeatmapDataset(Dataset):
     '''
     Preprocesses the data while loading them into the dataset
     '''
-    def __init__(self):
-       super().__init__()
+    def __init__(self, root_path, num_points=10000):
+        '''
+        NOTE: 
+            For the note selection model, we want the map described as follows: 
+                {Audio, General, Difficulty, Events, TimingPoints, TimeStamps, Objects},
+            where 
+                <Audio> - the preprocessed song the beatmap is created for, 
+                <General> and <Difficulty> are maps which contain their respective information in the osu file
+                <Events> is a list of breaks in the map
+                <TimingPoints> is a list of timing sections within the osu beatmap. 
+                <TimeStamps> is a list of time stamps in the song (in milliseconds from the beginning), 
+                <HitObjects> is a list of hitobjects corresponding to the given time stamp, 
+        NOTE2:
+            selection model encoder wants the sequence of time stamps and the sequence of corresponding hit objects (for teacher-forcing training)
+        
+        params:
+            <root_path> - path to root directory of the dataset
+            <num_points> - number of training points to load from the data set found in <root_path>, default: 10,000
+        '''
+        super().__init__()
+
+        #TODO: initialize a data structure where each element corresponds to the format described above
+
+        #create list which stores each tuple described above
+        data = []
+
+        #iterate over each beatmap in the training data from <root_path>
+        dct = {}
+        #apply preprocessing on the audio
+        #apply preprocessing on the beatmap
+        #combine results into one map
+
+
+        #convert list into pytorch tensor
+        data_t = tensor(data)
+        self.data = data_t
 
     def __len__(self):
         '''
         return the size of the dataset
         '''
-        #TODO: override len method 
-        pass
+        return self.data.size
 
     def __getitem__(self, i):
         '''
         retrieve the ith data sample in the dataset
         '''
-        #TODO: override getitem method so array style indexing is possible
-        pass
-def preprocess_audio():
+        return self.data[i]
+
+
+
+def preprocess_audio(path, dct, key="Audio"):
     '''
-    use librosa to compute mel-spectrograms
+    Compute mel-spectrogram using the librosa library
+
+    params:
+        path - relative path to the mp3 audio file for which we apply filtering on
+        dct - dictionary to store the result of filter application
+        key - the corresponding key our filter application should be mapped to in <dct> (SHOULD ALWAYS USE DEFAULT!!!!)
     '''
-    pass
+    #TODO: implement
+    dct[key] = "this is a placeholder"
 
 def process_beatmap(path):
-    map = {}
+    '''
+    Processes a .osu file into a dictionary containing 
+    '''
+    dct = {}
     with open(path, 'r', encoding='utf-8') as f:
         contents = f.readlines()[2:]
         i = 0 
@@ -42,27 +87,27 @@ def process_beatmap(path):
             match line:
                 case "[General]":
                     (lines_parsed, parsed_contents) = parse_gen_diff(contents[i+1:]) 
-                    map["General"] = parsed_contents
+                    dct["General"] = parsed_contents
                     i += lines_parsed
                 case "[Difficulty]":
                     (lines_parsed, parsed_contents) = parse_gen_diff(contents[i+1:]) 
-                    map["Difficulty"] = parsed_contents
+                    dct["Difficulty"] = parsed_contents
                     i += lines_parsed
                 case "[Events]":
                     (lines_parsed, parsed_contents) = parse_events(contents[i+1:])
-                    map["Events"] = parsed_contents
+                    dct["Events"] = parsed_contents
                     i += lines_parsed
                 case "[TimingPoints]":
                     (lines_parsed, parsed_contents) = parse_timingPoints_hitObjects(contents[i+1:])
-                    map["TimingPoints"] = parsed_contents
+                    dct["TimingPoints"] = parsed_contents
                     i += lines_parsed
                 case "[HitObjects]":
                     (lines_parsed, parsed_contents) = parse_timingPoints_hitObjects(contents[i+1:])
-                    map["HitObjects"] = parsed_contents
+                    dct["HitObjects"] = parsed_contents
                     i += lines_parsed
                 case _:
                     i += 1
-    print(map)
+    return dct
                     
 def parse_gen_diff(contents):
     '''
@@ -105,6 +150,23 @@ def parse_timingPoints_hitObjects(contents):
         parsed_contents.append(line)
     return (lines_parsed, parsed_contents)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#NOTE: below functions may be unnecessary
+
 def parse_hitObjects(contents):
     '''
     parse through the [HitObjects] Section of the .osu! file
@@ -139,22 +201,24 @@ def parse_hitobj_circle(info):
     '''
 
     '''
-    #TODO
+    #NOTE: may not be necessary
     pass
 
 def parse_hitobj_slider(info):
     '''
 
     '''
-    #TODO
+    #NOTE: may not be necessary
 
 def parse_hitobj_spinner(info):
     '''
 
     '''
-    #TODO
+    #NOTE: may not be necessary
 
-#example
+
+
+#example usage
 dir = os.path.dirname(__file__)
 filename = os.path.join(dir, '..', 'data', '2085341 BUTAOTOME - Street Journal', 'BUTAOTOME - Street Journal (Djulus) [Extra].osu')
 process_beatmap(filename)
