8cf855b5f9fff996f07c6212713b4e372256e6d9
Seb <sebastianpedrucochoi@hotmail.com>
2024-12-08 15:00:48 -0500

    template for postprocessing

---
+++ src/postprocessing.py
@@ -0,0 +1,5 @@
+#TODO: define functions which convert a sequence output from our model and format a .osu file with relevant info
+import os
+
+def create_map(song_path, song_name, bpm, timestamps, hitobjects):
+    pass
