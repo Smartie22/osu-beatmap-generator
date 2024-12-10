4e55b9ce61a8a86168fcfc3c4e0dabf48bedea27
Smartie22 <meikymeily2003@gmail.com>
2024-12-09 21:25:56 -0500

    updated postprocessing

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -1,14 +1,19 @@
 #TODO: define functions which convert a sequence output from our model and format a .osu file with relevant info
 import os
 import random
+import librosa
 
-def create_map(song_path, song_name, bpm, timestamps, hitobjects):
+def create_map(song_path, timestamps, hitobjects):
+
+    song = librosa.load(song_path)[0]
+    bpm = int(librosa.beat.beat_track(y=song)[0].item())
+
     with open("osu_generated_beatmap.osu", 'w') as map:
         # create the general section
         map.writelines(['osu file format v9\n',
                         '\n',
                         "[General]\n",
-                        f'AudioFilename: {song_name}\n',
+                        f'AudioFilename: {os.path.basename(song_path)}\n',
                         "AudioLeadIn: 0\n",
                         "PreviewTime: 100\n",
                         "Countdown: 0\n",
@@ -19,10 +24,10 @@ def create_map(song_path, song_name, bpm, timestamps, hitobjects):
         
         # create the metadata section
         map.writelines(["\n", "[Metadata]\n",
-                        f'Title:{song_name}\n',
+                        f'Title:{os.path.splitext(os.path.basename(song_path))[0]}\n',
                         "Artist:\n",
                         "Creator: Some Broken Neural Network\n",
-                        "Version:0\n",
+                        "Version:\n",
                         "Source:\n",
                         "Tags:\n"])
 
@@ -36,19 +41,21 @@ def create_map(song_path, song_name, bpm, timestamps, hitobjects):
                         "SliderTickRate:2\n"])
 
         # create the timing points section
-        map.writelines(["\n", "[TimingPoints]\n"])
+        # time,beatLength,meter,sampleSet,sampleIndex,volume,uninherited,effects
+        map.writelines(["\n", "[TimingPoints]\n",
+                       f"0,{int(60000/bpm)},4,0,0,100,0,0\n"])
 
 
         # create the hitobject section
         map.writelines(["\n", "[HitObjects]\n"])
         for ms, ho in zip(timestamps, hitobjects):
-            line: tuple[str] = ho.split(',')
+            line = ho.split(',')
             x, y = line[0], line[1]
             types = {'c': 0b00000001, 'l': 0b00000010, 's': 0b00001000}
             t = types[line[2]]
-            map.write(f'{x},{y},{ms},{t},0,{",".join(line[3:])}\n')
+            map.write(f"{x},{y},{ms},{t},0{',' + ','.join(line[3:]) if line[2] == 'l' else ''}\n")
 
-o = random.sample(["46,43,c,-1", "44,108,c,-1", "44,172,c,-1", "44,237,c,-1", "67,297,l,B|139:270|161:184", "193,82,l,B|256:53|322:81", "315,209,l,B|387:209", "255,236,c,-1", "196,208,l,B|123:208", "402,136,c,-1", "360,86,c,-1"], 10)
+o = random.sample(["46,43,c,-1", "44,108,c,-1", "44,172,c,-1", "44,237,c,-1", "67,297,l,B|139:270|161:184,1,130", "193,82,l,B|256:53|322:81,1,130", "315,209,l,B|387:209,2,65", "255,236,c,-1", "196,208,l,B|123:208,2,65", "402,136,c,-1", "360,86,c,-1"], 10)
 t = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-create_map('~\\..\\data\\0a7a8f8b7126cf33605db69928adcb48\\audio.opus', 'audio.opus',
-           100, t, o)
+file = os.path.join(os.path.dirname(__file__), "../test/audio.opus")
+create_map(file, t, o)
